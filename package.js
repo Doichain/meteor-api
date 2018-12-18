b8021f722c2aa442fa43dd2b793f80a4a0325726
Nico Krause <nico@le-space.de>
2018-12-18 21:09:25 +0400

    minor improvements

--- package.js
+++ package.js
@@ -1,56 +1,14 @@
 Package.describe({
   name: 'doichain-meteor-api',
-  version: '0.0.8',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
+  version: '0.0.6',
+  summary: 'Provides a Doichain REST API & webfrontend to an installed Doichain Node',
   git: 'https://github.com/Doichain/meteor-api.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-});
-
-Package.onUse(function(api) {
-  api.versionsFrom('1.8.0.1');
-  api.addFiles('private/version.json', 'server', { isAsset: true });
-  api.use('ecmascript@0.12.3');
-  api.use('underscore@1.0.10');
-  api.use('accounts-password@1.5.1');
-  api.use('alanning:roles@1.2.16');
-  api.use('aldeed:collection2@3.0.1');
-  api.use('aldeed:collection2-core@2.1.2');
-  api.use('aldeed:schema-deny@2.0.1');
-  api.use('aldeed:schema-index@3.0.0');
-  api.use('babel-runtime@1.3.0');
-  api.use('blaze-html-templates@1.1.2');
-  api.use('ddp-rate-limiter@1.0.7');
-
-  api.use('email@1.2.3');
-  api.use('es5-shim@4.0.0');
-  api.use('http@1.4.2');
-  api.use('less@2.8.0');
-  api.use('mdg:validated-method@1.2.0');
-  api.use('nimble:restivus@0.8.12');
-
-  api.use('planettraining:material-design-icons-font@2.2.3');
-  api.use('practicalmeteor:chai@2.1.0_1');
-  api.use('react-meteor-data@0.2.17');
-  api.use('rwatts:uuid');
-  api.use('sakulstra:aggregate');
-  api.use('session@1.2.0');
-  api.use('std:accounts-ui@1.3.3');
-  api.use('tracker@1.2.0');
-  api.use('universe:i18n@1.20.1');
-  api.use('vsivsi:job-collection@1.4.0');
-  api.use('zetoff:accounts-material-ui@0.0.15');
-
-
-
-  api.mainModule('doichain-api.js');
 });
 
 Npm.depends({
     '@babel/runtime':'7.2.0',
+    'simpl-schema':'1.5.3',
     'bcrypt': '1.0.3',
     'bitcore-message': '1.0.4',
     'bs58': '4.0.1',
@@ -73,14 +31,46 @@ Npm.depends({
     'react-router-private-route': '0.0.3',
     'scribe-js': '2.0.4',
     'secp256k1': '3.5.0',
-    'simpl-schema': '1.5.0',
     'standard-ecies': '1.0.0',
     'uuid': '3.3.2'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.8.0.1');
+  api.addFiles('private/version.json', 'server', { isAsset: true });
+  api.use('ecmascript@0.12.3');
+  api.use('underscore@1.0.10');
+  api.use('accounts-password@1.5.1');
+  api.use('alanning:roles@1.2.16');
+  api.use('babel-runtime@1.3.0');
+  api.use('blaze-html-templates@1.1.2');
+  api.use('ddp-rate-limiter@1.0.7');
+  api.use('email@1.2.3');
+  api.use('es5-shim@4.0.0');
+  api.use('http@1.4.2');
+  api.use('less@2.8.0');
+  api.use('mdg:validated-method@1.2.0');
+  api.use('nimble:restivus@0.8.12');
+  api.use('planettraining:material-design-icons-font@2.2.3');
+  api.use('practicalmeteor:chai@2.1.0_1');
+  api.use('react-meteor-data@0.2.17');
+  api.use('rwatts:uuid');
+  api.use('sakulstra:aggregate');
+  api.use('session@1.2.0');
+  api.use('std:accounts-ui@1.3.3');
+  api.use('tracker@1.2.0');
+  api.use('universe:i18n@1.20.1');
+  api.use('vsivsi:job-collection@1.4.0');
+  api.use('zetoff:accounts-material-ui@0.0.15');
+    api.use('aldeed:collection2@3.0.1');
+    api.use('aldeed:schema-deny@2.0.1');
+    api.use('aldeed:schema-index@3.0.0');
+  api.mainModule('doichain-api.js');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('doichain-api');
+  api.use('doichain-meteor-api');
   api.mainModule('doichain-api-tests.js');
 });
