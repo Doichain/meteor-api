ea2d6326d06e3cfd875a03a74138305acab490c3
Nico Krause <nico@le-space.de>
2018-12-19 10:56:30 +0400

    fixed packages

--- package.js
+++ package.js
@@ -6,13 +6,51 @@ Package.describe({
   documentation: 'README.md'
 });
 
+Package.onUse(function(api) {
+  api.versionsFrom('1.8.0.1');
+  api.addFiles('private/version.json', 'server', { isAsset: true });
+  api.use('ecmascript');
+  api.use('es5-shim');
+  api.use('babel-runtime');
+  api.use('vsivsi:job-collection@1.4.0','server');
+  api.use('alanning:roles@1.2.16','server');
+  api.use('aldeed:collection2@3.0.1','server');
+  api.use('aldeed:schema-deny@2.0.1','server');
+  api.use('aldeed:schema-index','server');
+  api.use('universe:i18n@1.20.1');
+
+    api.use('underscore@1.0.10',['client', 'server']);
+    api.use('accounts-password@1.5.1',['client', 'server']);
+
+    api.use('ddp-rate-limiter@1.0.7','server');
+    api.use('email@1.2.3','server');
+    api.use('http@1.4.2','server');
+    api.use('less@2.8.0','client');
+    api.use('mdg:validated-method@1.2.0');
+    api.use('nimble:restivus@0.8.12','server');
+    api.use('planettraining:material-design-icons-font@2.2.3','client');
+
+    api.use('react-meteor-data@0.2.16','server');
+    api.use('rwatts:uuid@0.1.0','server');
+    api.use('sakulstra:aggregate@1.4.3','server');
+    api.use('session@1.2.0');
+  //api.use('std:accounts-ui@1.3.3');
+
+
+  //api.use('zetoff:accounts-material-ui@0.0.15');
+  api.mainModule('doichain-api.js', 'server');
+  //  api.mainModule('client_main.js', 'client');
+  //  api.mainModule('client_main.js', 'client');
+  api.export('Doichain');
+});
+
+
 Npm.depends({
     '@babel/runtime':'7.2.0',
     'simpl-schema':'1.5.3',
     'bcrypt': '1.0.3',
     'bitcore-message': '1.0.4',
     'bs58': '4.0.1',
-    'chrome': '0.1.0',
     'classnames': '2.2.6',
     'crypto': '1.0.1',
     'crypto-js': '3.1.9-1',
@@ -35,37 +73,20 @@ Npm.depends({
     'uuid': '3.3.2'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.8.0.1');
-  api.addFiles('private/version.json', 'server', { isAsset: true });
-  api.use('ecmascript@0.12.3');
-  api.use('underscore@1.0.10');
-  api.use('accounts-password@1.5.1');
-  api.use('alanning:roles@1.2.16');
-  api.use('babel-runtime@1.3.0');
-  api.use('blaze-html-templates@1.1.2');
-  api.use('ddp-rate-limiter@1.0.7');
-  api.use('email@1.2.3');
-  api.use('es5-shim@4.0.0');
-  api.use('http@1.4.2');
-  api.use('less@2.8.0');
-  api.use('mdg:validated-method@1.2.0');
-  api.use('nimble:restivus@0.8.12');
-  api.use('planettraining:material-design-icons-font@2.2.3');
-  api.use('practicalmeteor:chai@2.1.0_1');
-  api.use('react-meteor-data@0.2.16');
-  api.use('rwatts:uuid@0.1.0');
-  api.use('sakulstra:aggregate@1.4.3');
-  api.use('session@1.2.0');
-  //api.use('std:accounts-ui@1.3.3');
-  api.use('tracker@1.2.0');
-  api.use('universe:i18n@1.20.1');
-  api.use('vsivsi:job-collection@1.4.0');
-  //api.use('zetoff:accounts-material-ui@0.0.15');
-    api.use('aldeed:collection2@3.0.1');
-    api.use('aldeed:schema-deny@2.0.1');
-    api.use('aldeed:schema-index@3.0.0');
-  api.mainModule('doichain-api.js');
+Package.onTest(api => {
+     api.use('practicalmeteor:chai@2.1.0_1');
+    // api.use([
+    //     'accounts-base',
+    //     'ecmascript',
+    //     'tinytest',
+    //     'random',
+    //     'test-helpers',
+    //     'oauth-encryption',
+    //     'ddp',
+    //     'accounts-password'
+    // ]);
+    //
+    // api.addFiles('accounts_tests_setup.js', 'server');
+    // api.mainModule('server_tests.js', 'server');
+    // api.mainModule('client_tests.js', 'client');
 });
-
-
