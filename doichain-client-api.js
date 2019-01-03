23eb6b04b32af99482ab90e68d7570b6018f0052
Nico Krause <nico@le-space.de>
2019-01-03 13:33:42 +0400

    minor fixes to enable testing

--- doichain-client-api.js
+++ doichain-client-api.js
@@ -1,7 +1,7 @@
 import {OptIns} from "./imports/api/opt-ins/opt-ins";
 import {Recipients} from "./imports/api/recipients/recipients";
-import {getHttpGET, getHttpGETdata, getHttpPOST} from "server/api/http";
-import {testLogging} from "imports/startup/server/log-configuration";
+import {getHttpGET, getHttpGETdata, getHttpPOST} from "./server/api/http";
+import {testLogging} from "./imports/startup/server/log-configuration";
 export let OptInsCollection = OptIns;
 export let RecipientsCollection = Recipients;
 export let httpGET = getHttpGET;
--- doichain-server-api.js
+++ doichain-server-api.js
@@ -3,8 +3,9 @@ export const name = 'doichain-meteor-api';
 
 import {OptIns} from "./imports/api/opt-ins/opt-ins";
 import {Recipients} from "./imports/api/recipients/recipients";
-import {getHttpGET, getHttpGETdata, getHttpPOST} from "server/api/http";
-import {testLogging} from "imports/startup/server/log-configuration";
+import {getHttpGET, getHttpGETdata, getHttpPOST} from "./server/api/http";
+import {testLogging} from "./imports/startup/server/log-configuration";
+
 export let OptInsCollection = OptIns;
 export let RecipientsCollection = Recipients;
 export let httpGET = getHttpGET;
