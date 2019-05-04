77b204cb96e859f3c09bdff952985efe7c0b9842
Nico Krause <nico@le-space.de>
2019-05-04 19:46:57 +0500

    re-enabled Accounts.config on api

--- imports/startup/server/useraccounts-configuration.js
+++ imports/startup/server/useraccounts-configuration.js
@@ -3,7 +3,7 @@ import {getSettings } from 'meteor/doichain:settings';
 
 //TODO please manage this over settings.json! since this causes security problems - forbidlientAccountCreation should be false
 //when installed on a dapp
-/*const accounts_sendVerificationEmail = getSettings('accounts.sendVerificationEmail',true);
+const accounts_sendVerificationEmail = getSettings('accounts.sendVerificationEmail',true);
 const accounts_forbidClientAccountCreation = getSettings('accounts.forbidClientAccountCreation',false);
 
 console.log(Accounts.config.toString())
@@ -11,7 +11,6 @@ Accounts.config({
     sendVerificationEmail: accounts_sendVerificationEmail,
     forbidClientAccountCreation: accounts_forbidClientAccountCreation
 });
-*/
 
 
 Accounts.emailTemplates.from=getSettings('Accounts.emailTemplates.from','doichain@le-space.de');
