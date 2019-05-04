b04465a4d4192f0761b7b66472c6aeafbf707908
Nico Krause <nico@le-space.de>
2019-05-04 19:49:01 +0500

    released version 0.1.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'doichain:doichain-meteor-api',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Provides a Doichain REST API & webfrontend to an installed Doichain Node',
   git: 'https://github.com/Doichain/meteor-api.git',
   documentation: 'README.md'
