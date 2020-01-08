2322fe671537dff68c82845677793a184c5b03f8
Nico Krause <nico@le-space.de>
2020-01-08 19:11:50 +0100

    version upgrade to v0.3.36

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'doichain:doichain-meteor-api',
-  version: '0.3.35',
+  version: '0.3.36',
   summary: 'Provides a Doichain REST API & webfrontend for Doichain Node',
   git: 'https://github.com/Doichain/meteor-api.git',
   documentation: 'README.md'
