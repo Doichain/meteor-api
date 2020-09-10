08a207f0e5bea3e80d66658dc674dc8929436307
Nico Krause <nico@le-space.de>
2020-09-10 11:34:17 +0300

    reading rpcpassword from doichain.conf to create dApp admin password by default

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import {Meta} from '../../api/meta/meta.js'
-import fs from 'fs';
+import fs from 'fs'
+import os from 'os'
+import propertiesReader from 'properties-reader'
 
 Meteor.startup(() => {
-
+  
   const rd = process.env.PWD;
   const version = fs.readFileSync(`${rd}/private/version.json`).toString();
 
@@ -12,14 +14,27 @@ Meteor.startup(() => {
     Meta.remove({key:"version"});
   }
 
-  console.log("version from file: ",version)
+    console.log("version from file: ",version)
    Meta.insert({key:"version", value: version});
   
   if(Meteor.users.find().count() === 0) {
+
+    let rpcpassword = 'password'
+    const homedir = os.homedir()
+    console.log('homedir',homedir)
+    const doichainConfPath = homedir+'/.doichain/doichain.conf'
+    try {
+      const properties = propertiesReader(doichainConfPath)
+      rpcpassword =  properties.get('rpcpassword')
+      console.log('password from rpcpassword',rpcpassword)
+    }catch(e){
+      console.log('problem reading doichain.conf in ',doichainConfPath)
+    }
+
     const id = Accounts.createUser({
       username: 'admin',
       email: 'admin@doichain.org',
-      password: 'password'
+      password: rpcpassword
     });
     Roles.addUsersToRoles(id, 'admin');
   }
