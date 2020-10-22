d87c71634ee7373a66e1573782002267f55ad0a8
Nico Krause <nico@le-space.de>
2020-10-22 15:20:19 +0500

    version upgrade to 0.5.0 to support Doichain core dc0.20.1.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'doichain:doichain-meteor-api',
-  version: '0.4.1',
+  version: '0.5.0',
   summary: 'Provides a Doichain REST API & webfrontend for Doichain Node',
   git: 'https://github.com/Doichain/meteor-api.git',
   documentation: 'README.md'
