0664c580b9f951a6b94f6a0ad73a1f6e7de39369
Nico Krause <nico@le-space.de>
2020-12-15 17:53:19 +0500

    0.5.10

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'doichain:doichain-meteor-api',
-  version: '0.5.8',
+  version: '0.5.10',
   summary: 'Implements the Doichain Protocoll and provides a Doichain RPC API for Doichain nodes',
   git: 'https://github.com/Doichain/meteor-api.git',
   documentation: 'README.md'
