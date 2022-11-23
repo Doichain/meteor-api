1e439b952c90d30deddd33528b1b6383d3a90b55
Nico Krause <inspiraluna@users.noreply.github.com>
2022-11-23 12:42:29 +0800

    release v0.5.22

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'doichain:doichain-meteor-api',
-  version: '0.5.21',
+  version: '0.5.22',
   summary: 'Implements the Doichain Protocoll and provides a Doichain RPC API for Doichain nodes',
   git: 'https://github.com/Doichain/meteor-api.git',
   documentation: 'README.md'
